7149acc3774b2f17397a2d606b62c47a75056711
Zell <dimasikniocem@gmail.com>
2023-11-29 18:01:47 +0300

    Добавьте файлы проекта.

---
+++ FamilyMember.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	public class FamilyMember
+	{
+		public int Age { get; set; }
+		public Gender Gender { get; set; }
+		public string FullName { get; set; }
+		public FamilyMember? Mother { get; set; }
+		public FamilyMember? Father { get; set; }
+		public FamilyMember[] GetGrandMother()
+		{
+			return new FamilyMember[] { Mother?.Mother, Father?.Mother };
+		}
+		public FamilyMember[] GetGrandFather()
+		{
+			return new FamilyMember[] { Mother?.Father, Father?.Father };
+		}
+		public FamilyMember? Myj { get; set; }
+		public FamilyMember? Jena { get; set; }
+
+		public FamilyMember GetMyVifu()
+		{
+			return Jena;
+		}
+		public FamilyMember GetMyHusband()
+		{
+			return Myj;
+		}
+
+	}
+}
+public enum Gender
+{
+	male,
+	female
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using OOP;
+public class Program
+{
+	public static void Main( )
+	{
+		FamilyMember GrantFatherOne = new FamilyMember()
+		{
+			FullName = "Oleg Olegovich Olegov",
+			Age = 17,
+			Gender = Gender.male
+
+		};
+		FamilyMember GrantFatherSecond = new FamilyMember()
+		{
+			FullName = "Ivan Ivanovich Ivanov",
+			Age = 16,
+			Gender = Gender.male
+
+		};
+		FamilyMember GrantMotherOne = new FamilyMember()
+		{
+			FullName = "Ianna Petrova Olegova",
+			Age = 47,
+			Gender = Gender.female
+
+		};
+		FamilyMember GrantMotherSecond = new FamilyMember()
+		{
+			FullName = "Oksana Timurovna Ivanova",
+			Age = 35,
+			Gender = Gender.female
+
+		};
+		FamilyMember Father = new FamilyMember()
+		{
+			FullName = "Batya",
+			Age = 35,
+			Gender = Gender.male,
+			Mother = GrantMotherOne,
+			Father = GrantFatherOne,
+			
+		};
+		
+		FamilyMember? Mother = new FamilyMember()
+		{
+			FullName = "Mama",
+			Age = 35,
+			Gender = Gender.female,
+			Mother = GrantMotherSecond,
+			Father = GrantFatherSecond,
+			Myj = Father
+
+		};
+		FamilyMember Son = new FamilyMember()
+		{
+			FullName = "Oleg",
+			Age = 35,
+			Gender = Gender.male,
+			Mother = Mother,
+			Father = Father,
+
+		};
+		Father.Jena = Mother;
+
+		/*
+		var GrandMothers = Son.GetGrandMother();
+		
+		var GrandFather = Son.GetGrandFather();
+
+		Console.WriteLine(GrandMothers[0]?.FullName);
+		Console.WriteLine(GrandMothers[1]?.FullName);
+		*/
+		var GetMyj = Mother.GetMyHusband();
+		var GetJena = Father.GetMyVifu();
+		Console.WriteLine(GetJena.FullName);
+		Console.WriteLine(GetMyj.FullName);
+	}
+}
+
